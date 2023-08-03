297e45adfb6938c5383afbe58136ecd1afe34f2e
DiborahKiptoon <jdkiptoon@gmail.com>
2023-08-03 21:30:20 +0300

    first commit

---
+++ variables.py
@@ -0,0 +1,19 @@
+#Two things that I love
+item_1 = "I love travelling"
+item_2 = "I love food"
+
+#Two reasons why you want to learn programming
+reason_1 = "I love coding"
+reason_2 = "I want to leverage my programming skills to create solutions"
+
+#Two things that give you motivation
+
+motivation_1 = "Music"
+motivation_2 = "Money"
+
+print(item_1)
+print(item_2)
+print(reason_1)
+print(reason_2)
+print(motivation_2)
+print(motivation_2)
