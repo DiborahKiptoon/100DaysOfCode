24d0c0d88d1ec50dbbd5a438dfadcc4e1f69f2ab
DiborahKiptoon <jdkiptoon@gmail.com>
2023-08-04 23:22:59 +0300

    commit 3

---
+++ Python_Operations.py
@@ -0,0 +1,30 @@
+a= 10
+b = 5
+
+#Addition
+c = a + b
+print(c)
+
+#Subtraction
+d = a - b
+print(d)
+
+#Multiplication
+e = a * b
+print(e)
+
+#Division
+f = a / b
+print(b)
+
+#Modulus
+g = a % b
+print(g)
+
+#Division floor
+h = a // b
+print(h)
+
+#Power
+i = a ** b
+print(i)
