85df7e3e476d648778e96c45f51e8d55179d55da
DiborahKiptoon <jdkiptoon@gmail.com>
2023-08-11 23:38:58 +0300

    commit 6

--- print and variables/Assign_var1.py
+++
@@ -1,8 +0,0 @@
-#About me
-my_name = "My name is Diborah"
-my_age = "I'm 23 years old"
-my_hobby = "I love travelling"
-
-print(my_name)
-print(my_age)
-print(my_hobby)
--- print and variables/Assign_var2.py
+++
@@ -1,8 +0,0 @@
-#School
-my_university = "I went to KCA University"
-my_graduation = "I graduated in 2022"
-my_course = "I studied Bsc. Software Development"
-
-print(my_university)
-print(my_graduation)
-print(my_course)
