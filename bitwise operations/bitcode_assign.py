a35a119e271c018d6ea29a6d9d97d0abb17fc842
DiborahKiptoon <jdkiptoon@gmail.com>
2023-08-11 23:10:12 +0300

    commit 6

---
+++ bitwise operations/bitcode_assign.py
@@ -0,0 +1,10 @@
+def DecimalToBinary(num):
+    return("{0:B}".format(int(num)))
+
+# Write codes to convert the following into Bits
+	#12
+    a = DecimalToBinary(12)
+    print(a)
+	#34
+	#65
+	#456
