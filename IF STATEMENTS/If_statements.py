1f39987214c786451e2195931f57c79cb435b5c8
DiborahKiptoon <jdkiptoon@gmail.com>
2023-08-16 23:16:07 +0300

    commit 1

---
+++ IF STATEMENTS/If_statements.py
@@ -0,0 +1,13 @@
+var_age = 17
+
+#if section
+if(var_age >=18):
+    print("Allow into the party")
+print("Go home, you are underage!")    
+
+
+#Shopping activity
+var_cloth = "Silk"
+if(var_cloth == "cotton"):
+    print("This is the right Fabric")
+print("The fabric is too soft")
---
+++ IF STATEMENTS/if...else_statement.py
@@ -0,0 +1,45 @@
+#2-1 decision if statement
+#It only represents one decision to be made
+
+#The syntax
+#if (creterria):
+#    statement/execution
+#else:
+#    statement/execution
+
+
+#math=78
+#phy=80
+#eng=73
+#marks=(math+phy+eng)
+# if section
+#if(marks>=280):
+    #print(math)
+    #print(phy)
+    #else:
+    #  print (eng)
+
+#House hunting
+#hse_a =10000
+#hse_b =15000
+#budget=12000
+
+#if section
+#if(budget<12000):
+#   print(hse_a)
+#   print("house is too expensive")
+
+#  Baby feeding scenario
+baby_age = "0.5 years"
+
+# If section
+if(baby_age >= "1 year"):
+    print("You can stop breast-feeding")
+    print("You can give the cow milk")
+
+else:
+    print("Breast milk is a must") 
+    print("The baby is still weaning")   
+
+
+
