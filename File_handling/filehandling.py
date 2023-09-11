1af91221de0891ed153c9075d935b80b0dad895c
DiborahKiptoon <jdkiptoon@gmail.com>
2023-09-11 21:45:22 +0300

    file handling

--- File_handling/filehandling.py
+++ File_handling/filehandling.py
@@ -4,4 +4,20 @@
 var_name = open("statements.txt", "w")
 var_name.write("The teacher wants us to do this on our own\n")
 var_name.write("Today's class was about file handling\n")
-var_name.write("This is our last excercise\n")
+var_name.write("This is the last excercise for today's class\n")
+
+var_name.close()
+
+file_read=open("statements.txt", "r")
+
+print(file_read.read())
+
+var_name = open("statements.txt", "a")
+var_name.write("I have received a call\n")
+var_name.write("I am proud of myself \n")
+
+var_name.close()
+
+file_add = open("statements.txt", "r")
+
+
