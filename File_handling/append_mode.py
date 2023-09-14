b3ff51055dd80dd345332d7e72d67d8ed10b4a40
DiborahKiptoon <jdkiptoon@gmail.com>
2023-09-14 21:46:37 +0300

    file handling

--- File_handling/append_mode.py
+++ File_handling/append_mode.py
@@ -1,3 +1,4 @@
 var_name = open("file1.txt", "a")
-var_name.write("I am a hardworking woman")
+var_name.write("I am a hardworking woman\n")
+var_name.write ("I'm a great person\n")
 var_name.close()
