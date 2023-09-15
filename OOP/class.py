61670c72fbf01d94c19192c7be22ade467acb28e
DiborahKiptoon <jdkiptoon@gmail.com>
2023-09-15 20:48:01 +0300

    Classes in Python

---
+++ OOP/class.py
@@ -0,0 +1,54 @@
+#Classes in Python
+#Syntax
+'''class class_name:
+      stateents
+      or
+      def func():
+         statements
+
+
+create class_obj
+call on class_obj             
+'''
+
+#create a class holding human attributes
+class human:
+
+    #features
+    feature_1 = 'legs'
+    feature_2 = 'eyes'
+    feature_3 = 'Breasts'
+    feature_4 = 'dimples'
+
+
+#creating an object
+anne = human()
+print(anne.feature_1)
+
+#Create class chicken
+class chicken:
+
+    #features
+    feat_1 = "Features"
+    feat_2 = 'Thighs'
+    feat_3 = 'wings'
+    feat_4 = 'break'
+
+    #chicken acyivities
+    act_1 = 'lay eggs'
+    act_2 = 'crow'
+    act_3 = 'sleep'
+
+ #Create a chicken object
+chick =  chicken()
+print(chicken.act_2)
+
+#Create class car
+class car:
+    #car parts
+    part_1 = 'dashboard'
+    part_2 = 'wheels'
+    part_3 = 'horse power'
+    part_4 = 'Bumper'
+
+    #
