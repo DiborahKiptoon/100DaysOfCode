e31b7cfcd4ca3318478da12c04498f2c681d7408
DiborahKiptoon <jdkiptoon@gmail.com>
2023-09-29 10:22:53 -0700

    lists

---
+++ Data Structures/lists.py
@@ -0,0 +1,8 @@
+
+#Concatinate
+#The process of combining two lists to make on whole new lists
+Fruit = ["Oranges", "Avocado", "Grapes"]
+
+#concatinate symbo is +
+new_fruits = Fruit + Fruits
+print(new_fruits)
